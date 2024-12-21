e21733f3f50b980eef8f68bb1432c60af4cc8aaf
changeme4585 <benscience@naver.com>
2024-12-21 17:58:54 +0900

    feat : 거래 기록을 저장하는 DB 테이블

---
+++ app/src/main/java/com/example/coinapp/TradeDB.java
@@ -0,0 +1,31 @@
+package com.example.coinapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class TradeDB extends SQLiteOpenHelper {
+    public static final String TABLE_NAME = "voca";
+    public static final String state = "state";
+    public static final String coinName = "coinName";
+    public static final String amount = "amount";
+    public TradeDB( Context context) {
+        super(context, "TradeDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+                state + " TEXT , " +
+                coinName + " TEXT, " +
+                amount + " TEXT);";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    } // 거래기록을 저장하는 db 테이블
+}
