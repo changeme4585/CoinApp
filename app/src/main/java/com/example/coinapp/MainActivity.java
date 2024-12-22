62366adbc441441e6ff573547ce755be05892f59
changeme4585 <benscience@naver.com>
2024-12-22 11:51:23 +0900

    feat : gpt 프래그먼트 추가

--- app/src/main/java/com/example/coinapp/MainActivity.java
+++ app/src/main/java/com/example/coinapp/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.coinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  TradeDB tradeDB;
     private Button buttonCoinList;
     private Button buttonOwnedCoins;
     private Button buttonTradeHistory;
+    private Button buttonGPT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // 위에서 수정한 레이아웃
 
+        tradeDB = new TradeDB(this);
+        SQLiteDatabase db = tradeDB.getReadableDatabase();
+        tradeDB.onCreate(db);
+
+
         buttonCoinList = findViewById(R.id.buttonCoinList);
         buttonOwnedCoins = findViewById(R.id.buttonOwnedCoins);
         buttonTradeHistory = findViewById(R.id.buttonTradeHistory);
+        buttonGPT = findViewById(R.id.buttonGPT);
 
         // 앱 시작 시 기본 페이지: CoinListFragment
         if (savedInstanceState == null) {
@@ -40,7 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
         buttonTradeHistory.setOnClickListener(v -> {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentContainer, new TradeHistoryFragment())
+                    .replace(R.id.fragmentContainer, new SearchCoinFragment())
+                    .commit();
+        });
+
+        buttonGPT.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentContainer, new GptFragment())
                     .commit();
         });
     }
