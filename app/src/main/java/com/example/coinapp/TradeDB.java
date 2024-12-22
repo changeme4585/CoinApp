2609c6357bbf5f75df3955894dbb8a2dc36cbca1
changeme4585 <benscience@naver.com>
2024-12-22 18:38:02 +0900

    refactor : 테이블 필드 alter

--- app/src/main/java/com/example/coinapp/TradeDB.java
+++ app/src/main/java/com/example/coinapp/TradeDB.java
@@ -9,7 +9,8 @@ public class TradeDB extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "voca";
     public static final String state = "state";
     public static final String coinName = "coinName";
-    public static final String amount = "amount";
+    public static final String amount = "amount"; //매매 수량
+    public static final String coinPrice = "coinPrice";  //매매 가격
     public TradeDB( Context context) {
         super(context, "TradeDB", null, 1);
     }
@@ -19,6 +20,7 @@ public class TradeDB extends SQLiteOpenHelper {
         String createTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
                 state + " TEXT , " +
                 coinName + " TEXT, " +
+                coinPrice + " TEXT, " +
                 amount + " TEXT);";
         db.execSQL(createTable);
     }
